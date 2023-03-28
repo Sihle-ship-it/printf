886836e210e2b5b93b9ad1e82d3772599654173e
Thabiso Magagula <37230838+Treasure-B@users.noreply.github.com>
2023-03-29 00:39:52 +0200

    Update main.h

--- main.h
+++ main.h
@@ -2,6 +2,22 @@
 #define our_printf
 #include <stdio.h>
 #include <stdarg.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+
 /**
  * struct specifier - struct specifier
  * @valid: the valid character.
