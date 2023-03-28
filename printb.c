18a0addebabaacca081cbc8c89be9ebdbe298caf
Treasure-B <thabiso.magagula@gmail.com>
2023-03-28 05:28:19 -0700

    print b

--- printb.c
+++ printb.c
@@ -1,75 +1,48 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
-* print_binary - Converts an unsigned int to binary and prints it
-* @n: unsigned int to be converted to binary
-*
-* Return: Number of characters printed
-*/
-int print_binary(unsigned int n)
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-int binary[32] = {0};
-int i = 0;
-int count = 0;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
-while (n > 0)
-{
-binary[i] = n % 2;
-n /= 2;
-i++;
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
 
-for (i = i - 1; i >= 0; i--)
-{
-putchar(binary[i] + '0');
-count++;
-}
-
-return (count);
-}
-
-/**
-* _printf - Custom printf function
-* @format: format string
-*
-* Return: Number of characters printed
-*/
-int _printf(const char *format, ...)
-{
-va_list args;
-int count = 0;
-const char *p;
-
-va_start(args, format);
-
-for (p = format; *p != '\0'; p++)
-{
-if (*p != '%')
-{
-putchar(*p);
-count++;
-continue;
-}
-
-switch (*++p)
-{
-case 'b':
-{
-unsigned int n = va_arg(args, unsigned int);
-count += print_binary(n);
-break;
-}
-
-default:
-putchar(*p);
-count++;
-break;
-}
-}
-
-va_end(args);
-
-return (count);
-}
 
