bca081868b866dbf015e8e5a29a4165b67573a09
Thabiso Magagula <37230838+Treasure-B@users.noreply.github.com>
2023-03-28 16:47:21 +0200

    Update main.h

--- main.h
+++ main.h
@@ -23,4 +23,10 @@ int print_percent(va_list args);
 int (*get_func(char x))(va_list args);
 int print_binary(unsigned int n);
 int my_printf(const char *format, ...);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
 #endif
